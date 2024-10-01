21929017b75a44f881f02c473714ac52cdfab21e
Robert Walton <rob.walton@arm.com>
2024-10-01 21:29:29 +0100

    Add python config

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -170,6 +170,20 @@ return {
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
+        pylsp = {
+          settings = {
+          pylsp = {
+            plugins = {
+              pycodestyle = {
+                maxLineLength = 120
+              },
+              flake8 = {
+                enable = true
+              }
+            }
+          }
+          }
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
